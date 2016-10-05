41119c21701f2d2f33ca70e9aabbf2908d5881f5
xuanchien <chien.study@gmail.com>
2016-10-05 16:58:23 +0900

    initial commit

---
+++ jaist_festival_2016/app.js
@@ -0,0 +1,45 @@
+var data = {
+	"don-ca-tai-tu": {
+		"embed": "https://www.youtube.com/embed/g8VHzPJe1mc",
+		"title": "Đờn ca tài tử (ドンカータイトゥ)",
+		"japanese": "ユネスコ無形文化遺産に登録されたベトナム南部の伝統民謡 <br />ドンカータイトゥはベトナム南部のメコンデルタ地方に伝わる伝統民謡。口頭伝承で伝わるこの民謡は命日やお祝い事等様々なイベントで用いられています。 <br />元々はフエの宮廷雅楽から派生したもので、19世紀の終わり頃にベトナム南部で広まり、南部を代表する民間芸能になりました。特徴的なのはったそうです。ドンカータイトゥは漢字では琴歌才子と表記しますが、琴に合わせて歌う才子(楽器も歌もできるという意味で使われる)という意味だそうです。一般の市民が集って、地面で座って農作業の後などに演奏する事が多く、プロの音楽家によるパフォーマンスというよりは一般の農民の娯楽として演奏されることが多かったこのドンカータイトゥですが、今では2000を超えるグループがこのドンカータイトゥを練習し、演奏しているそうです。ドンカータイトゥに使われるのは様々な形をした弦楽器。",
+		"english": "A musical art with both scholarly and folk roots, Đờn ca tài tử is an indispensable part of the spiritual activity and cultural heritage of the people of southern Viet Nam. The music and songs evoke the people’s life and work on the land and rivers of the Mekong Delta region. Performed at numerous events such as festivals, death anniversary rituals and celebrations, Đờn ca tài tử is thus intimately connected with other cultural practices and customs, oral traditions and handicrafts. The performers express their feelings by improvising, ornamenting and varying the ‘skeletal melody’ and main rhythmic patterns of these pieces. Đờn ca tài tử is played on a variety of different instruments, including the moon-shaped lute, two-stringed fiddle, sixteen-stringed zither, pear-shaped lute, percussion, monochord and bamboo flute. Its repertoire is based on twenty principal songs and seventy-two classical songs. The musical art is passed on through oral transmission, based on imitation, from master instrumentalists and singers to students. Musicians need to study for at least three years to learn the basic instrumental techniques and master the musical modes to express different moods and emotions. Vocal students study the traditional songs and learn to improvise subtly, using different ornamentation techniques."
+	},
+	"quan-ho": {
+		"embed": "https://www.youtube.com/embed/bHucAgOrN0M",
+		"title": "Quan họ (クアンホ)",
+		"japanese": "ベトナム北部のバクニン省とバクザン省が発祥の民謡です。<br/> 2009年にはユネスコの無形文化遺産にも登録されました。<br />昔から現出して以来、クアンホは作られたのが多いです。ですから一度聞きた限り、終わりずに聞きたいはずです。元々伝統的なクアンホは伴奏がない、普通クアンホは、男女・二人またはグループを作って歌います。4，5人から成る、男性のグループや女性のグループがつくられ、春（旧暦1～2月）と秋（旧暦8月）の村の祭りの時に集まり、歌い合いました。クアンホ民謡の言葉はあまり清くて、豊富です。それは工夫して作れた詩句・民謡、感情の完全なものです。しかし、現在に様々な形式を持つクアンホがだんだん向上しています。例えばどんな時でも、場所でも、独唱や合唱団やCD/DVD録音が演奏されて、祭りだけでなく大幅になってきます。重点は現代的にはクアンホを歌い返しばかりか、聴視者もいて両側がお互いに愛情を交換して、特殊な舞台芸術になります。心をうっとりさせるために、クアンホが楽器につけてサポートされます。でもクアンホ民謡の魂（特徴）を維持しなければならないが、その楽器は笛、ドラム及び一弦琴を含みます。",
+		"english": "The birthplace of these folk songs are in Bac Ninh Province and Bac Giang Province in northern. In 2009 it was also registered in the UNESCO's intangible cultural heritage. <br />The quan họ style originated in what is now Bắc Ninh Province and was first recorded in the 13th century, and has traditionally been associated with the spring festivals that follow the celebration of Tết (the Vietnamese New Year). Historically, the singing began on the evening before the festival, but today it is much more common for the singing to occur on the main day of the festival. In general, an initial \"challenge phrase\" from the known body of songs is sung by a pair of female singers, following which a pair of male singers will respond by selecting and singing a \"matching phrase\", which must repeat the melody of the challenge phrase. Once they are finished, the order is reversed, and the men will issue their own challenge phrase with a different melody. While in the past the singing was unaccompanied, it is common today for the singers to be accompanied by instruments, whether traditional Vietnamese instruments or modern ones such as electric keyboards."
+	},
+	"bai-choi": {
+		"embed": "https://www.youtube.com/embed/YthhIZw64AI",
+		"title": "Bài chòi (バイ　チョイ)",
+		"japanese": "誰もがバイチョイは登場を正確に知っていません。このゲームは、約400年前、フエ省でのすべての春祭りの時に演奏されたと言われています。そして、「バイチョイ」は、その人気を失い、祭りはかつて封建首都フエとクアンナム省で歴史的な町ホイアンを世界文化遺産として認識された後に復活したまでほとんど、忘れられていました。<br />各「バイチョイは「カードの10種類に割り当てられた別の名前で30チェスの駒、で構成されています。各カード上の3つのチェスの駒の名前が書かれており、プレイヤーは、彼らが購入したいカードの種類を選択します。ニャチャンCAI（バンカー）はチェスの駒の名前が書かれているカード、竹の棒を、外に与えた後、ゲームを開始するように描かれています。ディーラーがパートナーにすべてのバーを提供した後、ゲームが開始されます。 「ヒエウ」（M.C.として働く）がランダムボックスから竹の棒を引き抜くことにより、ゲームを行っています。スティックのそれぞれは、チェスの駒の名を冠する、およびヒエウは、彼が箱の外に引き出された竹の棒上のチェスの駒のそれぞれを表す短い民謡を歌います。<br />パートナーは、木製の棒上のチェスの駒のいずれかの民謡を聞くと、彼らは叫ぶと、チェックのための彼らのチェスの駒を表示する必要があります。彼らは小さな賞品と等価である黄色のバナーを、受信、およびパートナーのいずれかが同じバーの3右側のチェスの駒を持っているときにセットが終了します。勝者は、ゲームの最大の賞を取得します",
+		"english": "No one knows exactly when “Bai Choi” appeared. It is said this game was played during every Spring Festival in Thua Thien Hue Province around 400 years ago. Then “Bai Choi” lost its popularity and was almost forgotten, until the festival was revived after the former capital Hue and the historic town Hoi An in Quang Nam Province were recognized as world cultural heritage sites. <br/>Each “Bai Choi” comprises of 30 chess pieces with different names, assigned to 10 types of cards. On each card is written the names of three chess pieces, and the players will select the type of card they want to buy. After the nha cai (banker) gives out the cards, a bamboo stick, on which the name of a chess piece is written, is drawn to start the game. After “nha cai” (the dealer) delivers all the bars to the partners, the game starts. The “Hieu” (working as M.C.) conducts the game by randomly drawing out the bamboo sticks from a box. Each of the sticks bears the name of a chess piece, and the hieu sings the short folksongs representing each of the chess pieces on the bamboo sticks he has drawn out of the box. <br />When the partners hear the folksong of any of the chess pieces on their wooden bars, they have to shout out and show their chess piece for a check. They receive a yellow banner, which is equivalent to a small prize, and the set ends when any of the partners has 3 right chess pieces on the same bar. The winner gets the biggest prize in the game."
+	}
+}
+function loadDataForArt(artName){
+	var artData = data[artName];
+	$("#title").text(artData['title']);
+	$("#ja").html(artData['japanese']);
+	$("#en").html(artData['english']);
+	$("#youtube_embeded").attr("src", artData["embed"])
+}
+
+$(".change-language").on('click', function(event){
+	var target = $(event.currentTarget).data('lang');
+	$(".change-language").removeClass('selected');
+	$(event.currentTarget).addClass('selected');
+	$(".info-text").addClass("hidden");
+	$("#" + target).removeClass("hidden");
+})
+
+$(".select-art").on('click', function(event){
+	var target = $(event.currentTarget).data('target');
+	$(".select-art").removeClass("selected");
+	$(event.currentTarget).addClass('selected');
+
+	loadDataForArt(target);
+})
+
+loadDataForArt('don-ca-tai-tu');
